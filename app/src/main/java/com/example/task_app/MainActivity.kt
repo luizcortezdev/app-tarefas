fd6b5e442ffd472602a910dcd268ee451e3eb14f
luiz <luizcortezcontato@gmail.com>
2025-07-08 19:30:14 -0300

    teste

---
+++ app/src/main/java/com/example/task_app/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.task_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.SearchView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var searchView: SearchView
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        searchView = findViewById(R.id.search_view)
+        bottomNav = findViewById(R.id.bottom_nav)
+
+        // Fragmento inicial padrão (Home)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, HomeFragment())
+            .commit()
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_add -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, CadastroFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, BuscarFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
+
